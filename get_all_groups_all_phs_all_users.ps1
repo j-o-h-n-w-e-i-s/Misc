3fdffc61d6521e541863204e57b20d97d2527ada
j-o-h-n-w-e-i-s <81605274+j-o-h-n-w-e-i-s@users.noreply.github.com>
2021-05-25 04:11:12 -0700

    getting into source control

---
+++ get_all_groups_all_phs_all_users.ps1
@@ -0,0 +1,15 @@
+Clear-Host 
+Write-Host "This script gets all groups and all users in psd1.org"
+Write-Host "Example User: jweisenfeld@psd1.org"
+$UserCredential = Get-Credential
+Connect-MsolService -Credential $UserCredential
+# Get all psd1.org groups
+Write-Host "Running get_all_groups"
+$Today = Get-Date -Format "yyyy-MM-dd-'H'HH-'M'mm"
+Write-Host $Today   
+Get-MsolGroup -All | Export-Csv .\all-groups-$Today.csv
+#  Get all psd1.org users
+Write-Host "Running get_all_users"
+$Today = Get-Date -Format "yyyy-MM-dd-'H'HH-'M'mm"
+Write-Host $Today
+Get-MsolUser -All | Export-Csv .\all-users-$Today.csv
