382df183dec2f6459c6d2b901555224eeab29ffb
j-o-h-n-w-e-i-s <81605274+j-o-h-n-w-e-i-s@users.noreply.github.com>
2022-03-28 15:18:09 -0700

    Create search_groups.ps1
    getting this under source control

---
+++ search_groups.ps1
@@ -0,0 +1,5 @@
+write-host "There are a total of $($args.count) arguments"
+for ( $i = 0; $i -lt $args.count; $i++ ) {
+    write-host "Argument  $i is $($args[$i])"
+} 
+Get-MsolGroup -SearchString $args[0]
